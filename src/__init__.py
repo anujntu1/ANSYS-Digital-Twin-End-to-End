9db679af49f56e35f99d5d8085f8228e130bf521
Anuj Chauhan <anujntuchem@gmail.com>
2026-06-14 00:58:52 +0800

    Remove src and configs folders

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-# ansys-digital-twin-e2e
-# Build pipeline: TwinModelEvaluator, TwinRuntimeEvaluator, ROMDataGenerator, TwinValidator
-# Runtime pipeline: DigitalTwinPipeline, DigitalTwinStateMonitor, SensorDataProcessor
